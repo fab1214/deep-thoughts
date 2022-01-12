d479dd65df20130d1c5f0aed6eb3eba4dcb86247
fab1214 <88295561+fab1214@users.noreply.github.com>
2022-01-12 18:01:40 -0500

    set up thought page and profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,21 +1,47 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import ThoughtList from '../components/ThoughtList';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
+import FriendList from '../components/FriendList';
 
 const Profile = () => {
+  //retrieve username from URL using useParams() (storing it as userParameter);
+  const { username: userParameter } = useParams();
+
+  const { loading, data } = useQuery(QUERY_USER, {
+    variables: { username: userParameter }
+  });
+
+  const user = data?.user || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          {/* Viewing <usernames>'s profile. */}
+          Viewing {user.username}'s profile.
         </h2>
       </div>
 
       <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">{/* PRINT THOUGHT LIST  */}</div>
-
-        <div className="col-12 col-lg-3 mb-3">{/* PRINT FRIEND LIST */}</div>
+        <div className="col-12 mb-3 col-lg-8">
+          {/* pass props from the ThoughtList component (thoughts & title) to render a list of thoughts unique to user */}
+          <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+        </div>
+        <div className='col-12 col-lg-3 mb-3'>
+          {/* pass in FriendList props (username, friendCount, friends) */}
+        <FriendList 
+        username={user.username}
+        friendCount={user.friendCount}
+        friends={user.friends} 
+        />
+        </div>
       </div>
     </div>
   );
 };
-
 export default Profile;
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
+
+  const { id: thoughtId } = useParams();
+  const { loading, data  } = useQuery(QUERY_THOUGHT, {
+    //id property on variables object will become the $id parameter in the GraphQL query
+    variables: { id: thoughtId}
+  });
+  //check if there is data for thought. if there is assign to thought variable, if not create an empty array
+  const thought = data?.thought || [];
+
+  if (loading){
+    return <div>Loading...</div>
+  }
   return (
-    <div>
-      <div className="card mb-3">
-        <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
-            Username
-          </span>{' '}
-          thought on createdAt
-        </p>
-        <div className="card-body">
-          <p>Thought Text</p>
-        </div>
-      </div>
+<div>
+  <div className="card mb-3">
+    <p className="card-header">
+      <span style={{ fontWeight: 700 }} className="text-light">
+        {thought.username}
+        {/* {' ' } means space in a text block */}
+      </span>{' '}
+      thought on {thought.createdAt}
+    </p>
+    <div className="card-body">
+      <p>{thought.thoughtText}</p>
     </div>
+  </div>
+  {/* if reactionCount is > 0 , render ReactionList component and pass in reactions array as a prop */}
+  {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+</div>
+
   );
 };
 
